58dddea6328ca58cb8dd258bf79a35d7fdee6d93
Vinay Kumar <vinay.ks@c2info.com>
2022-07-07 16:16:02 +0530

    delete query to clear all the records from ST

--- Procedures/general queries.sql
+++ Procedures/general queries.sql
@@ -1,3 +1,10 @@
-select * from st_track_mst
-where c_doc_no = '000/21/6/233'
-and n_confirm = 0
+delete from st_track_tray_move; 
+update st_track_det set n_complete=2 where n_complete in (0,1);
+update st_track_pick set n_reject_qty=n_qty where n_qty-(n_reject_qty+n_confirm_qty)>0;
+update st_track_mst set n_complete=9 where n_confirm=1 and n_complete=0; 
+delete from tray_ledger where c_prefix<>'OT'; 
+delete from st_tray_lock; 
+delete from st_pick_backup;
+delete from st_track_in;
+update stock set n_hold_qty = 0, n_store_track_qty = 0 where n_hold_qty<>0;
+update stock_godown set n_hold_qty = 0, n_store_track_qty = 0 where  n_hold_qty<>0;
